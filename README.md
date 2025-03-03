b51791ce13de703d86458011c45f6ad37c2fc4f2
abyssnlp <rawatshaurya1994@gmail.com>
2025-03-03 08:27:10 +0100

    Add test deployment for flink application

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Flink CDC to Iceberg
 
 This project is a simple example of how to use Flink to consume CDC events from a Postgres database and write them to an Iceberg table.
 All components are running on a local Kubernetes cluster.
+
+```shell
+docker buildx build -f Dockerfile -t flink-cdc-iceberg:0.1 .
+docker tag flink-cdc-iceberg:0.1 abyssnlp/flink-cdc-iceberg:0.1 
+docker push abyssnlp/flink-cdc-iceberg:0.1
+```
--- src/main/java/com/github/abyssnlp/Main.java
+++ src/main/java/com/github/abyssnlp/Main.java
@@ -1,7 +1,27 @@
 package com.github.abyssnlp;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        KafkaSource<String> source = KafkaSource.<String>builder()
+                .setBootstrapServers("kluster-kafka-bootstrap.kafka.svc.cluster.local:9092")
+                .setTopics(List.of("postgres1.public.tester"))
+                .setGroupId("flink-cdc-iceberg-consumer")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .setProperty("enable.auto.commit", "false")
+                .build();
+
+        env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source").print();
+        env.execute("Flink CDC Iceberg");
     }
 }
