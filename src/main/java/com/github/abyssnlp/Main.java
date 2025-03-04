03b52d9387dc9b1929716b090a48ae69cf62eaeb
abyssnlp <rawatshaurya1994@gmail.com>
2025-03-04 10:35:38 +0100

    Add state backend on s3 and ha config for job manager

--- src/main/java/com/github/abyssnlp/Main.java
+++ src/main/java/com/github/abyssnlp/Main.java
@@ -6,16 +6,32 @@ import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        Map<String, String> envVars = System.getenv();
 
+        String kafkaBootstrapServers = envVars.get("KAFKA_BOOTSTRAP_SERVERS");
+        String kafkaGroupId = envVars.get("KAFKA_GROUP_ID");
+        String kafkaTopicsString = envVars.get("KAFKA_CDC_TOPICS");
+
+        if(kafkaBootstrapServers == null) {
+            throw new IllegalArgumentException("KAFKA_BOOTSTRAP_SERVERS environment variable is not set");
+        } else if(kafkaGroupId == null) {
+            throw new IllegalArgumentException("KAFKA_GROUP_ID environment variable is not set");
+        } else if(kafkaTopicsString == null) {
+            throw new IllegalArgumentException("KAFKA_TOPICS environment variable is not set");
+        }
+
+        List<String> kafkaTopics = Arrays.stream(kafkaTopicsString.split(",")).toList();
         KafkaSource<String> source = KafkaSource.<String>builder()
-                .setBootstrapServers("kluster-kafka-bootstrap.kafka.svc.cluster.local:9092")
-                .setTopics(List.of("postgres1.public.tester"))
-                .setGroupId("flink-cdc-iceberg-consumer")
+                .setBootstrapServers(kafkaBootstrapServers)
+                .setTopics(kafkaTopics)
+                .setGroupId(kafkaGroupId)
                 .setStartingOffsets(OffsetsInitializer.earliest())
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .setProperty("enable.auto.commit", "false")
