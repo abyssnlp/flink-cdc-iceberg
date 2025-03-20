ae7f010fdbfc5108141ec2c6966a5e93283cd3bf
abyssnlp <rawatshaurya1994@gmail.com>
2025-03-21 00:23:32 +0100

    Minor fixes

--- scripts/update_records.py
+++ scripts/update_records.py
@@ -75,11 +75,11 @@ if __name__ == "__main__":
 
     fake = faker.Faker()
     updater = PostgresUpdater(
-        dbname=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD"),
-        host=os.getenv("POSTGRES_HOST"),
-        port=os.getenv("POSTGRES_PORT"),
+        dbname=os.getenv("POSTGRES_DB", "cdciceberg"),
+        user=os.getenv("POSTGRES_USER", "postgres"),
+        password=os.getenv("POSTGRES_PASSWORD", "postgres"),
+        host=os.getenv("POSTGRES_HOST", "localhost"),
+        port=os.getenv("POSTGRES_PORT", 5432),
     )
 
     for i in range(100):
